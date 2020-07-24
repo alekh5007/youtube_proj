647778a9348c16bb5ba23e2ed7bfe744aba0bb17
alekh kumar <alekh5007@gmail.com>
2020-07-24 10:53:57 +0530

    Updates

---
+++ precache-manifest.378d924a497d30f54457032c6cdcedae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3610c619b1b962d8e910c811f9279220",
+    "url": "/youtube_proj/index.html"
+  },
+  {
+    "revision": "042ed44034551acd396b",
+    "url": "/youtube_proj/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "b5dff0e0bc44e42c8e7d",
+    "url": "/youtube_proj/static/css/main.b248d685.chunk.css"
+  },
+  {
+    "revision": "042ed44034551acd396b",
+    "url": "/youtube_proj/static/js/2.e41f4b0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/youtube_proj/static/js/2.e41f4b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dff0e0bc44e42c8e7d",
+    "url": "/youtube_proj/static/js/main.9d5d7ff1.chunk.js"
+  },
+  {
+    "revision": "758e5f3f844ce11c7b8a",
+    "url": "/youtube_proj/static/js/runtime-main.7450c23e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/youtube_proj/precache-manifest.378d924a497d30f54457032c6cdcedae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtube_proj/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
